2ff1bb38a5dbd5285b3acc9a8e53e88e7a5a9013
Satish Gaurav <deepsatflow@gmail.com>
2025-05-25 16:31:24 +0530

    fix modal api changes

--- analysis/modal_alphafold.py
+++ analysis/modal_alphafold.py
@@ -8,7 +8,7 @@
 import os
 from pathlib import Path
 
-from modal import App, Image, Mount
+from modal import App, Image
 
 GPU = os.environ.get("MODAL_GPU", "A10G")
 TIMEOUT = os.environ.get("MODAL_TIMEOUT", 20 * 60)
@@ -117,10 +117,9 @@ def score_af2m_binding(af2m_dict: str, target_len: int, binders_len: list[int]) 
 
 
 @app.function(
-    image=image,
+    image=image.add_local_dir(LOCAL_MSA_DIR, remote_path="/msas"),
     gpu=GPU,
     timeout=TIMEOUT,
-    mounts=[image.add_local_dir(LOCAL_MSA_DIR, remote_path="/msas")],
 )
 def alphafold(
     fasta_name: str,
